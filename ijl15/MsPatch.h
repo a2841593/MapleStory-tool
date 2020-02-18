a0d5417682253f52487aa11faee70545feb78c02
a2841593 <61189353+a2841593@users.noreply.github.com>
2020-02-18 19:49:42 +0800

    Update and rename ijl15/MsPatch.h to  ijl15/MsPatch.h

--- ijl15/MsPatch.h
+++ ijl15/MsPatch.h
@@ -4,7 +4,7 @@
 void No_Cap() {
 	#if MsVer == 113
 		DWORD No_CapAddr = 0x00B1CE28;
-		*(double*)No_CapAddr = 300000.0;
+		*(double*)No_CapAddr = 300000000.0;
 	#elif MsVer == 120
 		double *DamageCap = new double(300000.00);
 		DWORD v120No_CapAddr1 = 0x00867A6C;
